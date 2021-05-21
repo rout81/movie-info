2310a674f034bd3e9aaee9d79a52f8ad9e88f6da
satanrout <satyaranjanrout81@gmail.com>
2021-05-21 00:31:03 -0400

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
-import Header from "./components/header/header";
-import Homepage from "./pages/homepage/homepage";
-import Details from "./pages/detailspage/detailspage";
-import Footer from "./components/footer/footer";
+// import Header from "./components/header/header";
+// import Homepage from "./pages/homepage/homepage";
+// import Details from "./pages/detailspage/detailspage";
+// import Footer from "./components/footer/footer";
+
+const Header = lazy(() => import("./components/header/header"));
+const Homepage = lazy(() => import("./pages/homepage/homepage"));
+const Details = lazy(() => import("./pages/detailspage/detailspage"));
+const Footer = lazy(() => import("./components/footer/footer"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
-        <Route exact path="/" component={Homepage} />
-        <Route path="/:details" component={Details} />
-        <Footer />
+        <Suspense fallback={<div>loading</div>}>
+          <Header />
+          <Route exact path="/" component={Homepage} />
+          <Route path="/:details" component={Details} />
+          <Footer />
+        </Suspense>
       </div>
     </BrowserRouter>
   );
